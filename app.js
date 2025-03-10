40a8d0168961b666cef54bb03a117215320b1771
nadir <nadirelserhid@gmail.com>
2025-03-10 17:22:13 +0300

    hemet & cors hemet => adding headers and cors => for with PORT will use this API

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const logger = require("./middlewares/logger");
 const { notFound, errorHanlder } = require("./middlewares/errors");
 require("dotenv").config();
 const coonectToDB = require("./config/db");
+const path = require("path");
+const helmet =require("helmet");
+const cors = require("cors")
 
 //connect to mongodb
 coonectToDB();
@@ -10,14 +13,23 @@ coonectToDB();
 //init app
 const app = express();
 
+//Static Folder 
+app.use(express.static(path.join(__dirname,"images")))
+
 // Apply middleware
 app.use(express.json());
 app.use(express.urlencoded({extended :false}));
 // Use the logger middleware
 app.use(logger);
 
+//Hlemet
+app.use(helmet());
+
+//Set view Engine
 app.set('view engine', 'ejs');
 
+//cors policy
+app.use(cors());
 // Use the books route
 app.use("/api/books", require("./routes/books"));
 app.use("/api/author", require("./routes/authors"));
